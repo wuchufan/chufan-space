a6828e36131a28a8554224816f46fb44e64687e0
Jeffrey Wu <wuchufan@Jeffreys-MacBook-Pro.local>
2019-12-05 21:44:38 -0500

    clean up the code from design1--1, ready to write animation

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -5,46 +5,18 @@ import SideBar from '../../components/SideBar/SideBar';
 import Aux from '../Aux/Aux';
 
 class Layout extends Component {
-  state={
-    layoutWidth:'medium'
-  }
 
-  sideBarClickedHandler=(width)=>{
-    switch (width){
-      case 'medium':
-      this.setState({layoutWidth:'small'});
-      break
-      case 'small':
-      this.setState({layoutWidth:'medium'});
-      break
-      default:
-      break
-    }
-  }
 
   render() {
-    let sideBarStyle=[classes['side-bar']];
-    let containerStyle=[classes['main__container']];
-    switch(this.state.layoutWidth){
-      case 'medium':
-      sideBarStyle.push(classes['side-bar--width-medium']);
-      containerStyle.push(classes['main__container--width-medium']);
-      break;
-      case 'small':
-      sideBarStyle.push(classes['side-bar--width-small']);
-      containerStyle.push(classes['main__container--width-small']);
-      break;
-      default:
-      break;
-    }
+
 
 
     return (<Aux>
 
       <main className={classes['main']}>
 
-        <SideBar className={sideBarStyle.join(' ')} clicked={()=>this.sideBarClickedHandler(this.state.layoutWidth)}/>
-        <div className={containerStyle.join(' ')}>
+        <SideBar className={classes['side-bar']}/>
+        <div className={classes['main__container']}>
           <div className={classes['main__content']}>
             <NavBar/>
             {this.props.children}
